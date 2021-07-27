2208f97a90e189bfc3b557c7078de8676d18658d
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-27 16:55:22 -0400

    feat: add Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,27 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import commonStyles from '../../styles/common.module.scss';
+import styles from './Header.module.scss';
+
+export default function Header(): JSX.Element {
+  const router = useRouter();
+
+  const isCurrentPageIndex = router.asPath === '/';
+
+  return (
+    <header
+      className={`${styles.wrapper} ${
+        isCurrentPageIndex ? styles.hasMarginTop : ''
+      }`}
+    >
+      <div className={commonStyles.container}>
+        <Link href="/">
+          <a>
+            <img src="/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
