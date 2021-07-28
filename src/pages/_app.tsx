b1063ed30fc02d037a7eb8fb13cf5d66a468d623
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-28 15:18:23 -0400

    refactor: isCurrentPageIndex from _app to Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import Header from '../components/Header';
 
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+  const isCurrentPageIndex = router.asPath === '/';
+
   return (
     <>
-      <Header />
+      <Header isCurrentPageIndex={isCurrentPageIndex} />
       <Component {...pageProps} />
     </>
   );
