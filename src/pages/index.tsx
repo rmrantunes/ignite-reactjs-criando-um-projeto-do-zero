acdf700dae7c8af72724a85b28d34402c9731a4c
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-27 20:03:55 -0400

    fet: get post links from Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
 
+import { PostLink } from '../components/PostLink';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+import styles from './Home.module.scss';
 
 interface Post {
-  uid?: string;
+  slug: string;
   first_publication_date: string | null;
   data: {
     title: string;
@@ -16,21 +18,55 @@ interface Post {
 }
 
 interface PostPagination {
-  next_page: string;
+  next_page?: string;
   results: Post[];
 }
 
-interface HomeProps {
+interface HomePageProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomePageProps): JSX.Element {
+  return (
+    <main className={commonStyles.container}>
+      <nav className={styles.postLinkList}>
+        {postsPagination.results.map(post => (
+          <PostLink
+            key={post.slug}
+            author={post.data.author}
+            title={post.data.title}
+            subtitle={post.data.subtitle}
+            slug={post.slug}
+            publishedAt={post.first_publication_date}
+          />
+        ))}
+      </nav>
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    { fetch: ['post.title', 'post.subtitle', 'post.author'], pageSize: 8 }
+  );
 
-//   // TODO
-// };
+  const posts: Post[] = postsResponse.results.map(post => ({
+    first_publication_date: new Date(
+      post.first_publication_date
+    ).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+    }),
+    data: post.data,
+    slug: post.uid,
+  }));
+
+  return {
+    props: {
+      postsPagination: { results: posts, next_page: postsResponse.next_page },
+    },
+  };
+};
