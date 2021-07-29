2a0a0b63059eff36fa7491ac8ef337c948e1acfb
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-29 08:42:44 -0400

    feat: implement nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Nprogress from 'nprogress';
 import Header from '../components/Header';
 
 import '../styles/globals.scss';
@@ -8,8 +11,31 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const router = useRouter();
   const isCurrentPageIndex = router.asPath === '/';
 
+  function handleStart(): void {
+    Nprogress.start();
+  }
+
+  function handleStop(): void {
+    Nprogress.done();
+  }
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="/nprogress.css" />
+      </Head>
       <Header isCurrentPageIndex={isCurrentPageIndex} />
       <Component {...pageProps} />
     </>
