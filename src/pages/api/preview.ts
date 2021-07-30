85ecf3c22d8b0d2adbfdfb00e252b198a051052a
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-30 06:51:27 -0400

    feat: add Prismic preview routes

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Document } from '@prismicio/client/types/documents';
+import { getPrismicClient } from '../../services/prismic';
+
+function linkResolver(doc: Document): string {
+  if (doc.type === 'post') {
+    return `/post/${doc.uid}`;
+  }
+  return '/';
+}
+
+export default async function PrismicPreviewModeRoute(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const { token: ref, documentId } = req.query;
+  const redirectUrl = await getPrismicClient(req)
+    .getPreviewResolver(ref as string, documentId as string)
+    .resolve(linkResolver, '/');
+
+  if (!redirectUrl) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  res.setPreviewData({ ref });
+
+  res.write(`<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
+  <script>window.location.href = '${redirectUrl}'</script>
+  </head>`);
+
+  res.end();
+}
