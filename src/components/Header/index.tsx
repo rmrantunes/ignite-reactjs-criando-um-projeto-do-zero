85d19e1f54a0cf2b7fcc2271a42e280b794d0348
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-30 06:53:31 -0400

    feat: implement ExitPreviewModeButton in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link';
 
 import commonStyles from '../../styles/common.module.scss';
+import { ExitPreviewModeButton } from '../ExitPreviewModeButton';
 import styles from './Header.module.scss';
 
 interface HeaderProps {
   isCurrentPageIndex?: boolean;
+  isPreview: boolean;
 }
 
 export default function Header({
   isCurrentPageIndex,
+  isPreview,
 }: HeaderProps): JSX.Element {
   return (
     <header
@@ -16,12 +19,14 @@ export default function Header({
         isCurrentPageIndex ? styles.hasMarginTop : ''
       }`}
     >
-      <div className={commonStyles.container}>
+      <div className={`${commonStyles.container} ${styles.container}`}>
         <Link href="/">
           <a>
             <img src="/logo.svg" alt="logo" />
           </a>
         </Link>
+
+        {isPreview && <ExitPreviewModeButton />}
       </div>
     </header>
   );
