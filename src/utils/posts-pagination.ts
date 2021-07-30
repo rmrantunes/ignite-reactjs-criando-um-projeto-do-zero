3abbc3044056a4c973f2f49962218e092a169577
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-30 09:59:55 -0400

    feat: new postPagination fn getNextPreviousPosts

--- src/utils/posts-pagination.ts
+++ src/utils/posts-pagination.ts
@@ -1,4 +1,6 @@
+import Prismic from '@prismicio/client';
 import ApiSearchResponse from '@prismicio/client/types/ApiSearchResponse';
+import { DefaultClient } from '@prismicio/client/types/client';
 import { Post, PostPagination } from '../components/PostLinkList';
 
 export function getPostsPagination(
@@ -20,4 +22,57 @@ export async function loadMorePosts(
   const prismicResponse = await response.json();
 
   return getPostsPagination(prismicResponse);
+}
+
+export type NextPreviousPosts = {
+  previousPost?: null | {
+    uid: string;
+    title: string;
+  };
+
+  nextPost?: null | {
+    uid: string;
+    title: string;
+  };
+};
+
+export async function getNextPreviousPosts(
+  prismicClient: DefaultClient,
+  currentPostId: string
+): Promise<NextPreviousPosts> {
+  const olderPostResponse = await prismicClient.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      pageSize: 1,
+      after: currentPostId,
+      orderings: '[document.first_publication_date desc]',
+      fetch: ['post.title'],
+    }
+  );
+
+  const newerPostResponse = await prismicClient.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      pageSize: 1,
+      after: currentPostId,
+      orderings: '[document.first_publication_date]',
+      fetch: ['post.title'],
+    }
+  );
+
+  const nextPost = olderPostResponse.results?.[0]
+    ? {
+        uid: olderPostResponse.results[0]?.uid || null,
+        title: olderPostResponse.results[0].data.title,
+      }
+    : null;
+
+  const previousPost = newerPostResponse.results?.[0]
+    ? {
+        uid: newerPostResponse.results[0]?.uid || null,
+        title: newerPostResponse.results[0].data.title,
+      }
+    : null;
+
+  return { previousPost, nextPost };
 }
