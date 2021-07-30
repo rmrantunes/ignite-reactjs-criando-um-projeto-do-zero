9625daf8389fc41fc9ea2568e8c798e352f68001
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-30 10:17:19 -0400

    feat: new format fn formatFullDate

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -4,3 +4,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 export function formatDate(date: Date): string {
   return format(date, 'dd LLL yyyy', { locale: ptBR });
 }
+
+export function formatFullDate(date: Date): string {
+  return format(date, "dd LLL yyyy 'às' k:m", { locale: ptBR });
+}
